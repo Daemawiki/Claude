b5ca8fd01d65f442698897e3706605c549f59eb0
seung won 🏆 <tmddnjsrla99@gmail.com>
2025-02-02 14:05:04 +0900

    feat: DocumentDTOMapper 추가

--- src/main/java/com/daemawiki/external/web/rest/document/DocumentDTOMapper.java
+++ src/main/java/com/daemawiki/external/web/rest/document/DocumentDTOMapper.java
@@ -1,18 +1,32 @@
 package com.daemawiki.external.web.rest.document;
 
+import com.daemawiki.external.web.rest.document.dto.DocumentCreateForm;
+import com.daemawiki.internal.core.domain.model.dto.document.DocumentInternalDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
-import static org.mapstruct.ReportingPolicy.ERROR;
 import static org.mapstruct.ReportingPolicy.IGNORE;
 
 @Mapper(
         componentModel = SPRING,
-        unmappedTargetPolicy = ERROR,
+        unmappedTargetPolicy = IGNORE,
         unmappedSourcePolicy = IGNORE
 )
 public interface DocumentDTOMapper {
 
-
+    @Mapping(
+            target = "documentTitle",
+            source = "documentTitle"
+    )
+    @Mapping(
+            target = "categoryList",
+            source = "categoryList"
+    )
+    @Mapping(
+            target = "type",
+            source = "type"
+    )
+    DocumentInternalDTO toDocumentDTO(DocumentCreateForm source);
 
 }
