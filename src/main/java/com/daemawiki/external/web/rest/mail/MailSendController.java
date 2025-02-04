b8afaf4a6ab265505df5487dba94e7e4647bfaa9
seung won 🏆 <tmddnjsrla99@gmail.com>
2025-02-04 12:29:38 +0900

    refactor: 어노테이션 캡슐화 및 책임 분리

---
+++ src/main/java/com/daemawiki/external/web/rest/mail/MailSendController.java
@@ -0,0 +1,29 @@
+package com.daemawiki.external.web.rest.mail;
+
+import com.daemawiki.external.web.annotation.ui.MailRestApi;
+import com.daemawiki.internal.core.domain.model.primitive.user.personal.Email;
+import com.daemawiki.internal.core.usecase.mail.MailSendUseCase;
+import com.daemawiki.archive.daemawiki.domain.mail.model.type.MailType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Mono;
+
+@MailRestApi
+@RequiredArgsConstructor
+class MailSendController {
+
+    private final MailSendUseCase userMailSendUseCase;
+
+    @PostMapping("/send")
+    Mono<Void> send(
+            @RequestParam("target")
+            @JsonProperty("target")
+            Email target,
+            @RequestParam("type") MailType type
+    ) {
+        return userMailSendUseCase.send(target, type);
+    }
+
+}
---
+++ src/main/java/com/daemawiki/external/web/rest/mail/MailVerifyController.java
@@ -0,0 +1,32 @@
+package com.daemawiki.external.web.rest.mail;
+
+import com.daemawiki.external.web.annotation.ui.MailRestApi;
+import com.daemawiki.internal.core.domain.model.primitive.mail.AuthCode;
+import com.daemawiki.internal.core.domain.model.primitive.user.personal.Email;
+import com.daemawiki.internal.core.usecase.mail.MailVerifyUseCase;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import reactor.core.publisher.Mono;
+
+@MailRestApi
+@RequiredArgsConstructor
+class MailVerifyController {
+
+    private final MailVerifyUseCase userMailVerifyUseCase;
+
+    @PostMapping("/verify")
+    Mono<Void> verify(
+            @RequestParam("target")
+            @JsonProperty("target")
+            Email target,
+
+            @RequestParam("code")
+            @JsonProperty("code")
+            AuthCode code
+    ) {
+        return userMailVerifyUseCase.verify(target, code);
+    }
+
+}
