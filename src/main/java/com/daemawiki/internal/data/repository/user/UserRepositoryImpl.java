25a2e34156cbe6c5d8b2535f091fbe7f87ecb0d6
seung won 🏆 <tmddnjsrla99@gmail.com>
2025-03-05 09:49:33 +0900

    hotfix: 생성자 접근제어자 변경

--- src/main/java/com/daemawiki/internal/data/repository/user/UserRepositoryImpl.java
+++ src/main/java/com/daemawiki/internal/data/repository/user/UserRepositoryImpl.java
@@ -15,7 +15,7 @@ class UserRepositoryImpl extends UserAbstractRepository {
 
     private final MongoQueryUtils mongoQueryUtils;
 
-    private UserRepositoryImpl(
+    public UserRepositoryImpl(
             final UserMongoRepository userMongoRepository,
             final UserEntityMapper userEntityMapper,
             final MongoQueryUtils mongoQueryUtils
