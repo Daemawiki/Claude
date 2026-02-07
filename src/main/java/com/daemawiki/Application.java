ff525abcaec2bf6d499809b3ae80050f8340ad8d
ori0o0p <tmddnjsrla99@gmail.com>
2026-02-07 14:34:42 +0900

    필요 없는 주석 제거

--- src/main/java/com/daemawiki/Application.java
+++ src/main/java/com/daemawiki/Application.java
@@ -3,12 +3,10 @@ package com.daemawiki;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.security.config.annotation.rsocket.EnableRSocketSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableWebFlux
-//@EnableRSocketSecurity
 @EnableWebFluxSecurity
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -17,13 +15,4 @@ class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
-
-/*
- *
- *   {presentation}        {application}          {domain}       - Layer
- *       [form]    ->     [internal dto]    ->    [entity]       - Data flow
- *             (mapping)                (mapping)                - Mapping needed
- *
- */
